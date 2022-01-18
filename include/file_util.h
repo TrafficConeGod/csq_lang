ec65b54844b49279228e29293eaa51fb599a512c
TrafficConeGod <TrafficConeGod@github.com>
2022-01-18 13:14:30 -0500

    add more docs

--- include/file_util.h
+++ include/file_util.h
@@ -4,6 +4,17 @@
 
 using file = FILE;
 
+/**
+ * @brief Checks if a file exists
+ */
 bool file_exists(string path);
+
+/**
+ * @brief Get the size of a file
+ */
 size_t get_size_of_file(string path);
+
+/**
+ * @brief Open a file with string type
+ */
 file* custom_fopen(string path, string mode);
